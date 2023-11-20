ddae89b48650626283e7081a9bb15707f4a740f9
volkovich <volkoviysash@gmail.com>
2023-11-20 21:39:25 +0200

    Initial Commit

---
+++ PersonalPlayground/PersonalPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalPlaygroundApp.swift
+//  PersonalPlayground
+//
+//  Created by Александр Волкович on 20.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct PersonalPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
